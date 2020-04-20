537c7f844683a357ab97a93a9d41cb343289f80f
Danail Branekov <danailster@gmail.com>
2020-04-20 20:51:14 +0300

    Fix GLFW hinting

--- first-app/main.cpp
+++ first-app/main.cpp
@@ -18,10 +18,10 @@ int main() {
         return 1;
     }
 
-    //glfwWindowHint(GLFW_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_VERSION_MINOR, 2);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test window", NULL, NULL);
     if(!mainWindow) {
